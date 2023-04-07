1e26c8b39589a32d0c21af753f1836bed338520f
so306 <sohavni.singh@gmail.com>
2023-04-07 03:34:27 -0400

    unread senders showing upgit add .!

---
+++ config.py
@@ -0,0 +1,4 @@
+"""Insta485 development configuration."""
+
+# Root of this application, useful if it doesn't occupy an entire domain
+APPLICATION_ROOT = '/'
--- my_parser/__init__.py
+++ my_parser/__init__.py
@@ -1,2 +1,4 @@
 import flask
 app = flask.Flask(__name__)
+
+import my_parser.my_parser_file
